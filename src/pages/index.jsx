18d400c8aab90b0be409713fd2c83001675c83d6
AOZONE <faiz157akbar@gmail.com>
2024-06-27 15:49:10 +0700

    first commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 // Step 1: Import React
 import * as React from 'react'
-
+<script src="https://cdn.tailwindcss.com"></script>
 // Step 2: Define your component
 const IndexPage = () => {
   return (
@@ -74,6 +74,20 @@ const IndexPage = () => {
 </section>
   {/*END CALL TO ACTION*/}
   {/*PRODUCT LIST*/}
+  <a href="#" class="container grid grid-cols-3 gap-4 max-w-xl mx-auto py-8" className="group block">
+  <img
+    src="https://images.unsplash.com/photo-1592921870789-04563d55041c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+    alt=""
+    className="aspect-square w-full rounded object-cover"
+  />
+  <div className="mt-3">
+    <h3 className="font-medium text-gray-900 group-hover:underline group-hover:underline-offset-4">
+      Simple Watch
+    </h3>
+
+    <p className="mt-1 text-sm text-gray-700">$150</p>
+  </div>
+</a>
   {/*END PRODUCT LIST*/}
   {/*FOOTER*/}
   {/*END FOOTER*/}
